e82772444ff7866d634ecfbc5c178e5472b7c8e5
Georgi Karchev <georgikarcev@gmail.com>
2023-11-16 23:13:01 -0500

    Calculationg plants over area

--- console.js
+++ console.js
@@ -8,11 +8,34 @@ const area=PI*radius*radius;
 const plantSpace=0.8;
 const plantNumber=20;
 const maxComp=area/plantSpace;
-let weeks=2;
+let weeks=8;
 
 //Predict the plant growth after a specific number of weeks.
 
 let plantGrowth=(plantNumber*(2)**(weeks-1));
 console.log(plantGrowth);
+console.log(weeks-1)
 
+//area capacity
+console.log(` whole circular aria is: ${area} squared meeters`);
 
+// -----------------------
+ let maxPlantCapacity= (area/plantSpace);
+ console.log(`Maximum number of plants in the area is:  ${maxPlantCapacity}`);
+
+ if (plantGrowth>maxPlantCapacity*(plantGrowth/100)){
+    console.log("Pruned" ,weeks, plantGrowth);
+ } else if (plantGrouth>(50*maxPlantCapacity)/100 && plantGrouth<(80*maxPlantCapacity)/100){
+    console.log("Monitored");
+ } else if ( plantGrowth<50*maxPlantCapacity){
+    console.log("Planted");
+ }
+ else {
+    console.log("end");
+ }
+
+  
+    
+
+       
+  
